4dd64ff3c2e242d6c45caf8d0179ce54f5082858
RamachandiranVijayalakshmi <66218468+RamachandiranVijayalakshmi@users.noreply.github.com>
2020-06-06 00:57:24 +0530

    Update palindrom.py

--- palindrom.py
+++ palindrom.py
@@ -1,7 +1,9 @@
 #Palindrome
-name=input('enter your world:')
-a=name.lower()
-if a==a[::-1]:
-    print(a,'is palindrome')
-else:
-    print(a,'is not palindrome')
+def n(name):
+   a=name.lower()
+   if a==a[::-1]:
+       print(a,'is palindrome')
+   else:
+       print(a,'is not palindrome')
+d=input('enter your world:')
+n(d)
